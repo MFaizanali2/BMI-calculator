ac613f5a3dd0f95ce3a6a625efcf8b2948957487
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-06 23:14:21 +0500

    still working
    continue

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-let w = 73;
-let h = 177;
-let total = w / (h /100 * h/100);
-let round = total.toFixed(2)
-console.log(round)
+
+function calculate() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+    let first = document.getElementById("first");
+    let total = weight / (height / 100 * height / 100);
+    let round = total.toFixed(2);
+    if(round === " "){
+        first.innerText = "Please enter some value";
+    }else{
+        first.innerText = `Your Total BMI is = ${round}`;
+
+    }
+    
+}
